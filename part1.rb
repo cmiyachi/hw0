158357381b59b1d9dbbefd613689c8d1d0a86072
Chris M <chrisyogini@gmail.com>
2014-05-07 16:32:37 -0400

    more clean up

--- part1.rb
+++ part1.rb
@@ -23,20 +23,19 @@ def max_2_sum(array)
 
 end
 
-# third program - do any of the elements sum to "n"
-def sum_2_n?(arrayOfNumbers, n)
-  len = arrayOfNumbers.length
-  if (len == 0)
-    return false
-  elsif len == 1
-    return false
-  else
+# Define a method sum_to_n? which takes an array of integers and an 
+# additional integer, n, as arguments and returns true if any two distinct 
+# elements in the array of integers sum to n. An empty array or single 
+# element array should both return false.
+def sum_2_n?(array, n)
+    return false if array.empty?
+    return false if array.length == 1
     i = 0
     j = 0
-    while i < len
-      while j < len
+    while i < array.length
+      while j < array.length
         if i != j
-          if arrayOfNumbers[i]+arrayOfNumbers[j] == n
+          if array[i]+array[j] == n
             # puts i.to_s + " " + j.to_s + " " + n.to_s
             return true
           end
@@ -46,17 +45,16 @@ def sum_2_n?(arrayOfNumbers, n)
       i += 1
       j = 0
     end
-  end
-  return false
+    return false
 end
 
 my_array = [1,2,3,4,5,6,7,8,9,10]
-sum(my_array)
+puts sum(my_array)
 empty_array = Array.new
-sum(empty_array)
+puts sum(empty_array)
 
-max_2_sum(my_array)
-max_2_sum(empty_array)
+puts max_2_sum(my_array)
+puts max_2_sum(empty_array)
 
 puts sum_2_n?(my_array, 19)
 puts sum_2_n?(empty_array, 19)
